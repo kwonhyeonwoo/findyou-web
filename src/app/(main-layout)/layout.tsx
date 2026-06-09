0cd8ced821240c17cb73b283c093455abfbe855e
kwonhyeonwoo <khw19980926@gmail.com>
2026-06-09 10:29:14 +0900

    feat: floadting 버튼작업

--- src/app/(main-layout)/layout.tsx
+++ src/app/(main-layout)/layout.tsx
@@ -1,3 +1,4 @@
+import FloatingActionBtn from "@/components/common/FloatingActionBtn/FloatingActionBtn";
 import BottomTab from "@/components/layout/BottomTab/BottomTab";
 import MainHeader from "@/components/layout/MainHeader/MainHeader";
 import MainAuthProvider from "@/components/MainAuthProvider/MainAuthProvider";
@@ -12,8 +13,11 @@ export default function MainLayout({
     <MainAuthProvider>
       <div className="mx-auto relative flex min-h-screen w-full max-w-[480px] flex-col bg-white shadow-xl">
         <MainHeader />
-        <main className="flex-1 pb-20">{children}</main>
-        <BottomTab/>
+        <main className="flex-1 pb-20">
+          {children}
+          <BottomTab/>
+        </main>
+        <FloatingActionBtn/>
       </div>
     </MainAuthProvider>
   );
