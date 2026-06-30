a6679c74ba09bf33819c49b6214f62791ff7fafc
kwonhyeonwoo <khw19980926@gmail.com>
2026-06-30 10:16:55 +0900

    fix: 내역탭 조건문 변경, pathname으로 type이랑 비교

--- src/components/common/HistoryTab/HistoryTab.tsx
+++ src/components/common/HistoryTab/HistoryTab.tsx
@@ -2,16 +2,17 @@
 import { HISTORY_TAB } from "@/constants/history-constant";
 import Link from "next/link";
 import { useHistoryTab } from "./hooks/useHistoryTab";
+import { useParams, usePathname } from "next/navigation";
 
 function HistoryTab() {
-  const { currentTab, onCurrentTab } = useHistoryTab();
+  const { currentTab, pathname, onCurrentTab } = useHistoryTab();
   return (
     <div className="flex w-full items-center justify-between border-b border-b-[#E3E2E2]">
       {HISTORY_TAB.map(({ type, text, link }) => (
         <Link
           onClick={() => onCurrentTab(type)}
           href={link}
-          className={`flex flex-1 items-center justify-center p-4 ${currentTab === type && "border-b border-b-[#2A14B4]"} `}
+          className={`flex flex-1 items-center justify-center p-4 ${pathname.slice(9) === type && "border-b border-b-[#2A14B4]"} `}
           key={type}
         >
           <p className="text-[18px] font-medium">{text}</p>
--- src/components/common/HistoryTab/hooks/useHistoryTab.ts
+++ src/components/common/HistoryTab/hooks/useHistoryTab.ts
@@ -1,12 +1,15 @@
+import { usePathname } from "next/navigation";
 import { useState } from "react";
 
 export const useHistoryTab = () => {
+  const pathname = usePathname();
   const [currentTab, setCurrentTab] = useState<"request" | "application">(
     "request",
   );
   const onCurrentTab = (type: "request" | "application") => setCurrentTab(type);
 
   return {
+    pathname,
     currentTab,
     onCurrentTab,
   };
