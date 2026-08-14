fe810a8a7e3587f40fc3c90cbfd1fd1376f7dc45
kwonhyeonwoo <khw19980926@gmail.com>
2026-08-14 12:35:26 +0900

    fix:받은내역에 수락된지원자, 요청대기중인 지원자가 있을 경우에 UI변경, 카드 클릭하면 진행상황 페이지로 이동

--- src/components/template/ReceivedHistoryTemplate/ReceivedHistoryTemplate.tsx
+++ src/components/template/ReceivedHistoryTemplate/ReceivedHistoryTemplate.tsx
@@ -1,22 +1,34 @@
 'use client';
 import ReceivedCard from '@/components/Received/ReceivedCard/ReceivedCard';
 import useReceivedHistory from './hooks/useReceivedHistory';
+import { CustomStatus } from '@/interfaces/common.interface';
 
 export default function ReceivedHistoryTemplate() {
-  const { data, handleReceivedHistory } = useReceivedHistory();
+  const { data, handleReceivedHistory, handleAcceptedActive } =
+    useReceivedHistory();
   return (
     <div className="flex flex-col gap-5 pt-5 pb-10">
       <p className="text-[13px] text-[#464554]">
         내가 등록 한 헬퍼 글 {data?.length}
       </p>
       <div className="flex flex-col gap-4">
-        {data?.map((item) => (
-          <ReceivedCard
-            handleReceivedHistory={handleReceivedHistory}
-            data={item}
-            key={item.id}
-          />
-        ))}
+        {data?.map((item) => {
+          console.log('item', item.applications);
+          const acceptedApplication = item.applications.find(
+            (accepted) =>
+              accepted.status === CustomStatus.ACCEPTED ||
+              accepted.status === CustomStatus.COMPLETE_REQUESTED,
+          );
+          return (
+            <ReceivedCard
+              data={item}
+              acceptedApplication={acceptedApplication}
+              key={item.id}
+              handleAcceptedActive={handleAcceptedActive}
+              handleReceivedHistory={handleReceivedHistory}
+            />
+          );
+        })}
       </div>
     </div>
   );
